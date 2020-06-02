1e5cb58bc0df5b09098c18f4d883e94b88246ca7
Neetesh Keshari <neetesh.keshari@akqa.com>
2020-06-02 11:58:13 +0530

    Initial Commit
    Initial Commit

---
+++ lib/core/consts.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color greenColor = Color(0XFFFDEFE2);
+  static final Color greenLightColor = Color(0XFFFCF4EB);
+  static final Color redColor = Color(0XFFFE8F72);
+}
